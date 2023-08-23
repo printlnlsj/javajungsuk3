e8cb55ea4486f867d31db95558c69b1f3f69e3ff
LeeSeungJae <printlnlsj@gmail.com>
2023-08-23 22:31:53 +0900

    first commit

---
+++ src/ch6/BlockTest.java
@@ -0,0 +1,28 @@
+package ch6;
+
+/**
+ * 예제 6-27
+ */
+public class BlockTest {
+    // 클래스 초기화 블럭
+    static {
+        System.out.println("static { }");
+    }
+
+    // 인스턴스 초기화 블럭
+    {
+        System.out.println("{ }");
+    }
+
+    public BlockTest() {
+        System.out.println("생성자");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("BlockTest bt = new BlockTest();");
+        BlockTest bt = new BlockTest();
+
+        System.out.println("BlockTest bt2 = new BlockTest();");
+        BlockTest bt2 = new BlockTest();
+    }
+}
