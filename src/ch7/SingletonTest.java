af3a05fdf736ec4985b04bc7fea90fafb8472340
LeeSeungJae <printlnlsj@gmail.com>
2023-09-06 23:05:03 +0900

    first commit

---
+++ src/ch7/SingletonTest.java
@@ -0,0 +1,26 @@
+package ch7;
+
+/**
+ * 예제 7-14
+ */
+final class Singleton {
+    private static Singleton s = new Singleton();
+
+    private Singleton() {
+        // ...
+    }
+
+    public static Singleton getInstance() {
+        if (s == null) {
+            s = new Singleton();
+        }
+        return s;
+    }
+}
+
+public class SingletonTest {
+    public static void main(String[] args) {
+//        Singleton s = new Singleton(); // 에러! Singleton() has private access in Singleton
+        Singleton s = Singleton.getInstance();
+    }
+}
